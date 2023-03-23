2a810ac18073d46a66eb9617abad45ab27826b50
Teertho Kamol Goshami Likhon <littu323@gmail.com>
2023-03-23 20:16:10 +0600

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,21 @@
+# Define the function to perform the calculation
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+# Call the calculate function and print the result
+result = calculate(num1, num2, operator)
+print("Result: ", result)
